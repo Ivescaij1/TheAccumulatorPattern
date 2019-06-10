dd42a582f5ea77112f7b8f2c030c7ba5d04e4fdc
ivescai <caij1@rose-hulman.edu>
2019-06-10 17:53:38 +0800

    M5 T2 done

--- src/m5_more_graphical_accumulators.py
+++ src/m5_more_graphical_accumulators.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python. 
   
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Junfei Cai.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -98,7 +98,7 @@ def draw_squares_from_circle(n, circle, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -111,6 +111,25 @@ def draw_squares_from_circle(n, circle, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    circle.attach_to(window)
+    circle_center = circle.center
+    radius = circle.radius
+
+    x = circle_center.x
+    y = circle_center.y
+
+    for _ in range(n):
+        square_center = rg.Point(x, y)
+        square_side = 2 * radius
+        square = rg.Square(square_center, square_side)
+
+        square.attach_to(window)
+
+        x = x + radius
+        y = y + radius
+
+    window.render()
 
 
 def run_test_draw_circles_from_rectangle():
